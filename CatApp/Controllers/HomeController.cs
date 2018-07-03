1520ba7f0e0f491f45bde30d6bb7e9a2ea303da2
Hussain <umerhussa11n@gmail.com>
2018-07-03 09:49:57 +0100

    consuming the cat api
    consuming the API

---
+++ CatApp/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using CatApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace CatApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static HttpClient client = new HttpClient();
+        
+
+        public static async Task<CategoriesViewModel> GetAllCategories()
+        {
+            var model = new CategoriesViewModel();
+            try
+            {
+                string uri = "http://thecatapi.com/api/categories/list";
+                var response = await client.GetStreamAsync(uri);
+                model = DeserializeXMLToObject<CategoriesViewModel>(response, "categories");
+            }
+            catch (Exception ex)
+            {
+                var exception = ex;
+            }
+            return model;
+        }
+
+        public static T DeserializeXMLToObject<T>(Stream stream, string startingNode)
+        {
+            T returnObject = default(T);
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+                using (var xmlReader = XmlReader.Create(stream))
+                {
+                    xmlReader.ReadToFollowing(startingNode); 
+                    returnObject = (T)serializer.Deserialize(xmlReader);
+                }
+            }
+            catch (Exception ex)
+            {
+                var exception = ex.InnerException;
+            }
+            return returnObject;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await GetAllCategories();
+
+            var selectListItems = categories.Categories.Select(x => new SelectListItem() { Value = x.Id.ToString(), Text = x.Name }).ToList();
+            categories.lstCategories = selectListItems;
+
+            return View(categories);
+        }
+
+        public async Task<JsonResult> GetImagesForCategory(string Category)
+        {
+            var uri = "http://thecatapi.com/api/images/get?format=xml&amp;results_per_page={10}&amp;category=" + Category;
+
+         
+            var response = await client.GetStringAsync(uri);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(response);
+
+            XmlNodeList ImageUrlNL = doc.GetElementsByTagName("url");
+
+            List<string> ImageUrls = new List<string>();
+            if (ImageUrlNL.Count <= 1)
+            {
+                foreach (XmlNode node in ImageUrlNL)
+                {
+                    ImageUrls.Add(node.InnerText);
+                }
+
+            }
+
+            return Json(ImageUrls);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CatApp/Models/CategoriesViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CatApp.Models
+{
+    [XmlRoot("categories")]
+    public class CategoriesViewModel
+    {
+        public CategoriesViewModel()
+        {
+            _categories = new List<Category>();
+            _lstCategories = new List<SelectListItem>();
+        }
+
+
+        private List<Category> _categories;
+        [XmlElement("category")]
+        public List<Category> Categories
+        {
+            get { return _categories; }
+            set { _categories = value; }
+        }
+
+        private List<SelectListItem> _lstCategories;
+        public List<SelectListItem> lstCategories
+        {
+             get { return _lstCategories;  }
+            set { _lstCategories = value;  }
+        }
+
+
+
+        public string SelectedCategory { get; set; }
+
+        public class Category 
+        {
+            [XmlElement("id")]
+            [Key]
+            public int Id { get; set; }
+
+            [XmlElement("name")]
+            public string Name { get; set; }
+        }
+
+    }
+}
